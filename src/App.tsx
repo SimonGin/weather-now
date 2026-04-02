9b66157234a2e2c39a642a5b3cdf912ced21c5bf
SimonGin <tt26419@gmail.com>
2026-04-02 17:23:50 +0700

    ui: add header title + search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { IoSettingsOutline } from "react-icons/io5";
-import { FaChevronDown } from "react-icons/fa";
+import { FaChevronDown, FaSearch } from "react-icons/fa";
 
 const App = () => {
   return (
@@ -9,8 +9,25 @@ const App = () => {
           <img src="logo.svg" alt="" />
           <button className="bg-gray-700 w-32 rounded-xl flex items-center justify-between px-4">
             <IoSettingsOutline size={20} />
-            <h1 className="font-semibold">Units</h1>
+            <h1 className="font-medium">Units</h1>
             <FaChevronDown />
+          </button>
+        </div>
+        <h1 className="text-6xl font-bold text-center my-10">
+          How's the sky looking today?
+        </h1>
+        <div className="flex justify-center gap-4">
+          {/* Search Bar */}
+          <div className="flex flex-row items-center gap-3 w-2xl bg-gray-700 px-4 py-3 rounded-xl">
+            <FaSearch />
+            <input
+              className="w-full focus:outline-none"
+              type="text"
+              placeholder="Search for a place..."
+            />
+          </div>
+          <button className="bg-blue-600 px-4 py-3 rounded-xl font-medium ">
+            Search
           </button>
         </div>
       </div>
